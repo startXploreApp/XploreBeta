f8f912885cdd178b1e00739f29092e030d5effd3
PierreLEDEZ <p5ledez@gmail.com>
2022-07-09 10:07:18 +0200

    Change sharings

--- src/parts/litepaper/ResourceSharing.js
+++ src/parts/litepaper/ResourceSharing.js
@@ -12,12 +12,73 @@ const SharingCard = styled(Column)`
     padding: 4rem 0.5rem;
     gap: 0.5rem;
     color: ${props => props.isProject ? "white": "black"};
+    border-left: ${props => props.borderLeft ? "2px solid #000" : "none"};
+    border-right: ${props => props.borderRight ? "2px solid #000" : "none"};
     background: ${props => props.backgroundColor};
 `;
 
 const SharingCardTitle = styled.h5`
     text-align: center;
 `;
+
+const sharings = [
+    {
+        title: ["Trésor", "final"],
+        subtitle: "10%",
+        backgroundColor: "#B9DFC7",
+        isProject: false,
+        borderRight: false,
+        borderLeft: false
+    },
+    {
+        title: ["Récompense", "phase 4"],
+        subtitle: "13%",
+        backgroundColor: "#96CFAB",
+        isProject: false,
+        borderRight: false,
+        borderLeft: false
+    },
+    {
+        title: ["Récompense", "phase 3"],
+        subtitle: "13%",
+        backgroundColor: "#73BF8F",
+        isProject: false,
+        borderRight: false,
+        borderLeft: false
+    },
+    {
+        title: ["Récompense", "phase 2"],
+        subtitle: "13%",
+        backgroundColor: "#50AF73",
+        isProject: false,
+        borderRight: true,
+        borderLeft: false
+    },
+    {
+        title: ["Trésorerie"],
+        subtitle: "10%",
+        backgroundColor: "#408C5C",
+        isProject: true,
+        borderRight: false,
+        borderLeft: true
+    },
+    {
+        title: ["Marketing"],
+        subtitle: "20%",
+        backgroundColor: "#306945",
+        isProject: true,
+        borderRight: false,
+        borderLeft: false
+    },
+    {
+        title: ["Team", "X'PLORE"],
+        subtitle: "21%",
+        backgroundColor: "#20462E",
+        isProject: true,
+        borderRight: false,
+        borderLeft: false
+    }
+]
 
 const ResourceSharing = () => {
     return (
@@ -26,42 +87,25 @@ const ResourceSharing = () => {
                 <h3>Répartition des ressources</h3>
                 <Column style={{ width: "100%", gap: "0", padding: "0"}}>
                     <div style={{ width: "100%", display: "grid", gridTemplateColumns: "10% 13% 13% 13% 10% 20% 21%"}}>
-                        <SharingCard backgroundColor="#B9DFC7">
-                            <SharingCardTitle>
-                                Trésor final
-                            </SharingCardTitle>
-                            <h2>10%</h2>
-                        </SharingCard>
-                        
-                        <SharingCard backgroundColor="#96CFAB">
-                            <SharingCardTitle>Récompense <br/>phase 4</SharingCardTitle>
-                            <h2>13%</h2>
-                        </SharingCard>
-                        
-                        <SharingCard backgroundColor="#73BF8F">
-                            <SharingCardTitle>Récompense <br/>phase 3</SharingCardTitle>
-                            <h2>13%</h2>
-                        </SharingCard>
-                        
-                        <SharingCard style={{ borderRight: "2px solid #000" }} backgroundColor="#50AF73">
-                            <SharingCardTitle>Récompense <br/>phase 2</SharingCardTitle>
-                            <h2>13%</h2>
-                        </SharingCard>
-                        
-                        <SharingCard style={{ borderLeft: "2px solid #000" }} isProject backgroundColor="#408C5C">
-                            <SharingCardTitle>Trésorerie</SharingCardTitle>
-                            <h2>10%</h2>
-                        </SharingCard>
-                        
-                        <SharingCard isProject backgroundColor="#306945">
-                            <SharingCardTitle>Marketing</SharingCardTitle>
-                            <h2>20%</h2>
-                        </SharingCard>
-                        
-                        <SharingCard isProject backgroundColor="#20462E">
-                            <SharingCardTitle>Team <br/>X'PLORE</SharingCardTitle>
-                            <h2>21%</h2>
-                        </SharingCard>
+                        { sharings.map(element => (
+                            <SharingCard
+                                    isProject={element.isProject}
+                                    backgroundColor={element.backgroundColor}
+                                    borderLeft={element.borderLeft}
+                                    borderRight={element.borderRight}>
+                                <SharingCardTitle>
+                                    {element["title"].length === 1 ?
+                                        element["title"] :
+                                        element["title"].map(title_elt => (
+                                            [title_elt, <br/>]
+                                        ))
+                                    }
+                                </SharingCardTitle>
+                                <h2>
+                                    {element.subtitle}
+                                </h2>
+                            </SharingCard>
+                        ))}
                     </div>
                     <div style={{ color: "white", backgroundColor: "#1F2B3A", width: "100%", display: "grid", gridTemplateColumns: "49% 51%" }}>
                         <Row style={{ borderRight: "2px solid #000", justifyContent: "center"}}>
