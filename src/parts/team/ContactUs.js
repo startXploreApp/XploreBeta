778af374579443c42a793d58b021b765d29d8d3f
s7clauza <s7clauza@enib.fr>
2022-08-29 15:31:37 +0200

    team page finished

--- src/parts/team/ContactUs.js
+++ src/parts/team/ContactUs.js
@@ -1,6 +1,9 @@
 import Column from "../../components/Column"
 import Panel from "../../components/Panel"
 import styled from "styled-components";
+import Button from "../../components/Button";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPaperPlane } from "@fortawesome/free-solid-svg-icons";
 
 const Input = styled.input`
     border-radius : 5px;
@@ -16,9 +19,6 @@ const TriangleWrapper = styled.div`
     margin-right: 0; 
     position: 'relative';
     z-index: -1;
-    ::after{
-        background-color:"black"
-    }
 `;
 
 export const ContactUs = () => {
@@ -29,12 +29,16 @@ export const ContactUs = () => {
                 <Input placeholder="Nom" />
                 <Input type="email" placeholder="Mail" />
                 <textarea rows={4} style={{ width: "100%", borderRadius: "5px", padding: "0.5rem", border: "1px solid #6D757D", fontSize: "1rem", fontFamily: "Inter" }} placeholder="Message" />
+                <Button style={{ zIndex: "2", display: "flex", justifyContent: "center", alignItems: "center", alignContent: "center" }}>
+                    <h6>Envoyer</h6>
+                    <FontAwesomeIcon icon={faPaperPlane} size={"xl"} />
+                </Button>
             </Column>
             <TriangleWrapper>
-                <svg preserveAspectRatio="xMidYMin meet" style={{ position: 'absolute', top: 0, right: 0 }} width="683" height="100vh" viewBox="0 0 683 423" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <svg preserveAspectRatio="xMidYMin meet" style={{ position: 'absolute', top: 0, right: 0 }} width="100vw" height="200vh" viewBox="0 0 683 423" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M683 423V0L0 423H683Z" fill="#1F2B3A" />
                 </svg>
-                <svg preserveAspectRatio="xMidYMin meet" style={{ position: 'absolute', top: 0, right: 0 }} width="423" height="100vh" viewBox="0 0 423 420" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <svg preserveAspectRatio="xMidYMin meet" style={{ position: 'absolute', top: 0, right: 0 }} width="30vw" height="100vh" viewBox="0 0 423 420" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path opacity="0.3" d="M423 0H0L423 420V0Z" fill="black" />
                 </svg>
             </TriangleWrapper>
